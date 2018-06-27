e96a18b06c7514c79f54391d1bbbda2ade1d6d01
abecel22 <abe2211@gmail.com>
2018-06-27 07:47:03 -0500

    Added main components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
+import { LogsComponent } from './components/logs/logs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LogFormComponent,
+    LogsComponent
   ],
   imports: [
     BrowserModule
